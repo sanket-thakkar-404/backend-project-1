b22130d0c3b56320b987b3edcfcbd5c339715b21
sanket-thakkar-404 <sanketthakkar500@gmail.com>
2025-10-23 21:39:46 +0530

    edit and delete function made

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-
+// ya home screen ka route ha jisma hame pata chala ga ki hamna kon konse file rakhe ha
 app.get('/', (req, res) => {
   fs.readdir('./files', (err, file) => {
     if (err) {
@@ -27,13 +27,13 @@ app.get('/', (req, res) => {
     res.render('index', { files: allFiles });
   });
 })
-
+// ya hame new files bana ma help kara ga ki jo hame files bane ha jo uska data store kar ka usko file banya ga or uska data usma show kara ga
 app.post('/create', (req, res) => {
   fs.writeFile(`./files/${req.body.title.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join('')}.txt` , req.body.details , (err)=>{
     res.redirect('/')
   });
 })
-
+// ya hame file ko pura pane ma help karta ha ki hamere file ka data kya kya ha
 app.get('/file/:filename', (req, res) => {
   fs.readFile(`./files/${req.params.filename}`,"utf-8",(err, fileData) => {
     if (err) {
@@ -44,7 +44,56 @@ app.get('/file/:filename', (req, res) => {
   });
 })
 
+// ya hame edit page ma la jana ka kam karta ha 
+app.get('/edit/:filename', (req, res) => {
+  // res.send('your in edit routes')
+  fs.readFile(`./files/${req.params.filename}`,"utf-8",(err, fileData) =>{
+    if(err){
+      console.log(err)
+      return;
+    }
+     res.render('edit' , {filename : req.params.filename , fileData : fileData}) 
+  })
+ 
+})
 
+// ya hame edit file ko rename or content ko bhi new name karna ma help karta ha 
+app.post('/edits', (req, res) => {
+
+const oldPath = `./files/${req.body.oldTitle}.txt`;
+const newpath = `./files/${req.body.title.split(' ').map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase()).join('')}.txt`
+ fs.rename(oldPath, newpath, (err)=>{
+   if (err) {
+      console.log('Rename error:', err);
+      return res.send('Error renaming file');
+    }
+
+  const newContent = req.body.details && req.body.details.trim() !== '' ?req.body.details : req.body.oldDetails;
+  
+  fs.writeFile(newpath , newContent , ()=>{
+    if (err) {
+      console.log('Write error:', err);
+      return res.send('Error updating file content');
+     }
+     res.redirect('/')
+  })
+ })
+})
+
+// ya hame file ko delete karna ma help karta ha
+app.get('/delete/:filename', (req, res) => {
+  // res.send('your in edit routes')
+  fs.unlink(`./files/${req.params.filename}`,(err) =>{
+    if (err) {
+      console.log('Delete error:', err);
+      return res.send('Error deleting file');
+    }``
+     res.redirect('/') 
+  })
+ 
+})
+
+// ya hame port baata ha ki ham ko port ma ha
 app.listen(port, (req, res) => {
   console.log(`Your server is Running is port ${port}`)
 })
